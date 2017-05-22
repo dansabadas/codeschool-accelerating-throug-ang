a9bdd30632a554f641d06363b0a4531b509c5161
Dan Sabadis <dan_sabadas@hotmail.com>
2017-05-22 16:36:27 +0300

    ngfor and ngif samples

--- AcceleratingThroughAngular/app/main.js
+++ AcceleratingThroughAngular/app/main.js
@@ -11,20 +11,35 @@ var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 var AppComponent = (function () {
     function AppComponent() {
         this.heading = "Ultra Racing Schedule";
-        this.race = {
-            "id": 1,
-            "name": "Daytona Thunderdome",
-            "date": new Date('2512-01-04T14:00:00'),
-            "about": "Race through the ruins of an ancient Florida battle arena.",
-            "entryFee": 3200
-        };
+        this.races = [{
+                "id": 1,
+                "name": "Daytona Thunderdome",
+                "date": new Date('2512-01-04T14:00:00'),
+                "about": "Race through the ruins of an ancient Florida battle arena.",
+                "entryFee": 3200,
+                "isRacing": false
+            }, {
+                "id": 2,
+                "name": "San Francisco Ruins",
+                "date": new Date('2512-07-03T20:00:00'),
+                "about": "Drift down the streets of a city almost sunk under the ocean.",
+                "entryFee": 4700,
+                "isRacing": true
+            }, {
+                "id": 3,
+                "name": "New York City Skyline",
+                "date": new Date('2512-07-12T21:00:00'),
+                "about": "Fly between buildings in the electronic sky.",
+                "entryFee": 4300,
+                "isRacing": false
+            }];
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'racing-app',
-        template: "\n    <h1>{{ heading }}</h1>\n    <h2>{{race.name}}</h2>\n    <p>{{race.date}}</p>\n    <p>{{race.about}}</p>\n  "
+        template: "\n    <h1>{{heading}}</h1>\n  <ul>\n    <li *ngFor=\"let race of races\">\n      <h2>{{race.name}}</h2>\n      <p>{{race.date}}</p>\n      <p>{{race.about}}</p>\n<button *ngIf=\"!race.isRacing\" >Enter Race</button>\n    <h3 *ngIf=\"race.isRacing\" >Already Racing</h3>\n    </li>\n  </ul>\n  "
     })
 ], AppComponent);
 var AppModule = (function () {
--- AcceleratingThroughAngular/app/main.ts
+++ AcceleratingThroughAngular/app/main.ts
@@ -5,21 +5,42 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 @Component({
     selector: 'racing-app',
     template: `
-    <h1>{{ heading }}</h1>
-    <h2>{{race.name}}</h2>
-    <p>{{race.date}}</p>
-    <p>{{race.about}}</p>
+    <h1>{{heading}}</h1>
+  <ul>
+    <li *ngFor="let race of races">
+      <h2>{{race.name}}</h2>
+      <p>{{race.date}}</p>
+      <p>{{race.about}}</p>
+<button *ngIf="!race.isRacing" >Enter Race</button>
+    <h3 *ngIf="race.isRacing" >Already Racing</h3>
+    </li>
+  </ul>
   `
 })
 class AppComponent {
     heading = "Ultra Racing Schedule"
-    race = {
+    races = [{
         "id": 1,
         "name": "Daytona Thunderdome",
         "date": new Date('2512-01-04T14:00:00'),
         "about": "Race through the ruins of an ancient Florida battle arena.",
-        "entryFee": 3200
-    }
+        "entryFee": 3200,
+        "isRacing": false
+    }, {
+        "id": 2,
+        "name": "San Francisco Ruins",
+        "date": new Date('2512-07-03T20:00:00'),
+        "about": "Drift down the streets of a city almost sunk under the ocean.",
+        "entryFee": 4700,
+        "isRacing": true
+    }, {
+        "id": 3,
+        "name": "New York City Skyline",
+        "date": new Date('2512-07-12T21:00:00'),
+        "about": "Fly between buildings in the electronic sky.",
+        "entryFee": 4300,
+        "isRacing": false
+    }];
 }
 
 @NgModule({
